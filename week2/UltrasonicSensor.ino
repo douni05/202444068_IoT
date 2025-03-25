679432a3c8b2bd83b5347a0a2649aaedb5d13059
Doyun <163100839+douni05@users.noreply.github.com>
2025-03-25 22:45:33 +0900

    Update UltrasonicSensor.ino

--- week2/UltrasonicSensor.ino
+++ week2/UltrasonicSensor.ino
@@ -1,3 +1,5 @@
+// 초음파 센서를 이용한 거리 측정 및 LED 점멸 (Arduino)
+
 #define TRIG 13 // TRIG 판
 #define ECHO 12 // ECHO 판
 
@@ -20,13 +22,16 @@ void loop()
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2); // delay(2)
   digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(10); //10 마이크로초 동안 HIGH 유지 (초음파 발사)
   digitalWrite(TRIG, LOW);
   
+   // 초음파가 물체에 반사되어 돌아오는 시간 측정
   duration = pulseIn(ECHO, HIGH); // 343mls
+
+  // 거리 계산 (음속 343m/s = 34300cm/s -> 1us당 0.0343cm 이동)
+  distance = duration * 17 / 1000;
   
-  distance = duration * 17 / 1000; 
-  Serial.println(distance );
+  // 시리얼 모니터에 거리 출력
   Serial.print("\nDistance : ");
   Serial.print(distance);
   Serial.println(" CM");
