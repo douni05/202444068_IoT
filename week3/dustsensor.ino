50fbd27063185526c47daa23dec01a3e2318de77
Doyun <163100839+douni05@users.noreply.github.com>
2025-03-25 23:09:23 +0900

    Update dustsensor.ino

--- week3/dustsensor.ino
+++ week3/dustsensor.ino
@@ -1,3 +1,6 @@
+// 미세먼지 센서를 이용한 아날로그 값 측정 (Arduino)
+// A0 핀에서 먼지 센서 값을 읽고 시리얼 모니터에 출력
+
 int Vo = A0;
 int V_LED = 2;
 
@@ -12,7 +15,7 @@ void setup() {
 void loop() {
   digitalWrite(V_LED, LOW);
   delayMicroseconds(280);
-  Vo_value = analogRead(Vo);
+  Vo_value = analogRead(Vo);   // 센서 값 읽기 (아날로그 입력)
   delayMicroseconds(40);
   digitalWrite(V_LED, HIGH);
   delayMicroseconds(9680);
